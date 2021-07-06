165ffe1a372af5f3b0cc85b0d247573f466183e0
Umang Gupta <umanggupta.iet@gmail.com>
2021-07-06 18:08:38 +0530

    added action cable for chatbox

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  resources :messages, only: [:create]
+  mount ActionCable.server, at: '/cable'
 end
